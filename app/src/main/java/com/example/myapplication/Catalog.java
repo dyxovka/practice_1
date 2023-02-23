96f6dd77868ed2f03ff0e9cbd73e5a396d9098ea
dyxovka <varia.mazurovaaa@gmail.com>
2023-02-24 02:26:12 +0300

    передача информации из одной активити в другую и наоборот

--- app/src/main/java/com/example/myapplication/Catalog.java
+++ app/src/main/java/com/example/myapplication/Catalog.java
@@ -2,13 +2,53 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.example.myapplication.databinding.ActivityCatalogBinding;
 
 public class Catalog extends AppCompatActivity {
+    static String name_to;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_catalog);
+
+        ActivityCatalogBinding activityCatalogBinding = ActivityCatalogBinding.inflate(getLayoutInflater());
+        View view = activityCatalogBinding.getRoot();
+        Bundle arguments = getIntent().getExtras();
+        String name = arguments.get("name").toString();
+        String phone = arguments.getString("phone");
+        System.out.println(name);
+        name_to = name;
+        TextView textView1 = findViewById(R.id.textView5);
+        activityCatalogBinding.textView5.setText(name_to);
+        textView1.setText(name);
+
+        setContentView(view);
+        Intent intent1 = new Intent(this, MainActivity.class);
+
+
+
+
+        activityCatalogBinding.buttonGotovo1.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+
+                intent1.putExtra("name", name_to);
+                intent1.putExtra("done", "Вы просмотрели каталог");
+                intent1.putExtra("phone", phone);
+                startActivity(intent1);
+            }
+        });
+
+
     }
+
+
 }
--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,13 +3,17 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.renderscript.ScriptGroup;
+import android.telephony.PhoneNumberFormattingTextWatcher;
 import android.util.Log;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 
@@ -33,6 +37,11 @@ public class MainActivity extends AppCompatActivity  {
 
         ActivityMainBinding  activityMainBinding = ActivityMainBinding.inflate(getLayoutInflater());
         View view = activityMainBinding.getRoot();
+        // создание объекта Intent для запуска второй активности
+        Intent intent = new Intent(this, Catalog.class);
+        activityMainBinding.editTextTextPersonName.addTextChangedListener(new PhoneNumberFormattingTextWatcher());
+
+
 
         activityMainBinding.buttonGotovo.setOnClickListener(new View.OnClickListener() {
 
@@ -40,7 +49,14 @@ public class MainActivity extends AppCompatActivity  {
             public void onClick(View view) {
                 Toast toast = Toast.makeText(getApplicationContext(), "Ну, нажали", Toast.LENGTH_SHORT);
                 toast.show();
+                String name = activityMainBinding.editTextTextPersonName.getText().toString();
+                String phone = activityMainBinding.editTextPhone.getText().toString();
                 Log.i(TAG, "нажали программно");
+                // передача объекта с ключом "name" и значением "именя"
+                intent.putExtra("name", name);
+                intent.putExtra("phone", phone);
+// запуск SecondActivity
+                startActivity(intent);
             }
         }
 
@@ -52,6 +68,23 @@ public class MainActivity extends AppCompatActivity  {
 
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Bundle arguments = getIntent().getExtras();
+        try {
+            String name_from = arguments.get("name").toString();
+            TextView editText = findViewById(R.id.editTextTextPersonName);
+            editText.setText(name_from);
+            String phone_from = arguments.get("phone").toString();
+            TextView editText_phone = findViewById(R.id.editTextPhone);
+            editText_phone.setText(phone_from);
+            String done = arguments.get("done").toString();
+            TextView editText_done = findViewById(R.id.textView_done);
+            editText_done.setText(done);
+        } catch (NullPointerException e){
+            System.out.println();
+        }
 
-
+    }
 }
